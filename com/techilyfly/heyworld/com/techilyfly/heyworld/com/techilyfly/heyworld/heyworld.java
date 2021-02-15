4314bb2dc594a09744480eed322da1ee047748fd
AbdurRahmanG <AbdurRahmanGOfficial@gmail.com>
2021-02-15 17:48:19 +0530

    Initial Commit

---
+++ com/techilyfly/heyworld/com/techilyfly/heyworld/com/techilyfly/heyworld/heyworld.java
@@ -0,0 +1,7 @@
+package com.techilyfly.heyworld;
+
+public class heyworld{
+    public static void main(String[] args) {
+        System.out.println("Hey World, Welcome to the Java Field!");
+    }
+}
